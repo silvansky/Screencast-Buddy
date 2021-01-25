9345493b339e2a7c5e29c0b444407639da52fa90
Valentine Silvansky <v.silvansky@gmail.com>
2021-01-25 10:01:27 +0300

    Initial Commit

---
+++ Screencast Buddy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Screencast Buddy
+//
+//  Created by Valentine Gorshkov on 25.01.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Screencast Buddy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Screencast Buddy
+//
+//  Created by Valentine Gorshkov on 25.01.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
